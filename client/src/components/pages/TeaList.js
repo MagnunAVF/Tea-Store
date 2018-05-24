01608243cb5783554fe8b40d65c5810932511c9f
Magnun A V Furtado <magnunavf@gmail.com>
2018-05-24 14:34:18 -0700

    feat[client]: Create teas fetch

--- client/src/components/pages/TeaList.js
+++ client/src/components/pages/TeaList.js
@@ -4,19 +4,65 @@ import { Link } from 'react-router-dom'
 import Tea from './../widgets/Tea'
 import TeaDetail from './../widgets/TeaDetail'
 
-class TeaList extends React.Component {
-    render(){
-      return(
-        <div>
-          <h1>Chás</h1>
-          <h3>Tipo: { this.props.match.params.type }</h3>
-          <Tea />
-          <TeaDetail />
+const API_URL = "http://localhost:5000/available_teas"
 
-          <Link to='/'>Voltar</Link>
-        </div>
-      )
-    }
+class TeaList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      teas: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchTeas();
+  }
+
+  fetchTeas() {
+    console.log("fetching ...");
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(json => this.setState({ teas: json.teas }))
+  }
+
+  renderTeas() {
+    const teas = this.state.teas;
+    const emptyList = teas.length === 0;
+
+    return (
+      <div>
+        { emptyList ? (
+          <div><p>Sem chás no momento! </p></div>
+        ) : (
+          <div>
+            {
+              teas.map((tea) => {
+                return(<li key={tea.id}>{tea.name}</li>)
+              })
+            }
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Chás</h1>
+        <h3>Tipo: { this.props.match.params.type }</h3>
+
+        <Tea />
+        <TeaDetail />
+
+        <h3>Teas List</h3>
+        { this.renderTeas() }
+
+        <Link to='/'>Voltar</Link>
+      </div>
+    )
+  }
 }
 
 export default TeaList
