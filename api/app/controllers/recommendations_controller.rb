44f71b4c4c6f07f847e208be075a2a87f83e0a92
Magnun A V Furtado <magnunavf@gmail.com>
2018-05-24 17:34:05 -0700

    refactor[backend]: Improve recommendations controller code reuse

--- api/app/controllers/recommendations_controller.rb
+++ api/app/controllers/recommendations_controller.rb
@@ -2,31 +2,24 @@ class RecommendationsController < ApplicationController
   include TeasFetcher
 
   def available_teas
-    begin
-      teas = fetch_teas(ENV['TEAS_API_URL'], ENV['AUTHORIZATION_TOKEN'])
-    rescue StandardError
-      error = "Internal Recommendation API Error"
-      selected_teas = []
-    else
-      error = "-"
-      selected_teas = teas_in_stock(teas)
-    ensure
-      render json: json_response(selected_teas, error)
-    end
+    external_api_response = use_external_api
+
+    render json: external_api_response
   end
 
   def teas_by_type
-    tea_type = params[:tea_type]
+    external_api_response = JSON.parse(use_external_api)
 
-    begin
-      teas = fetch_teas(ENV['TEAS_API_URL'], ENV['AUTHORIZATION_TOKEN'])
-    rescue StandardError
-      error = "Internal Recommendation API Error"
-      selected_teas = []
+    tea_type = params[:tea_type]
+    error = external_api_response['error']
+
+    if error != '-'
+      render json: external_api_response
     else
-      error = "-"
-      selected_teas = filter_teas_by_type(teas, tea_type)
-    ensure
+
+      teas = external_api_response['teas']
+      selected_teas = multi_param_filter(teas, [tea_type])
+
       render json: json_response(selected_teas, error)
     end
   end
@@ -44,16 +37,30 @@ class RecommendationsController < ApplicationController
     selected_teas
   end
 
-  def filter_teas_by_type(teas, type)
+  def multi_param_filter(teas, types_list)
     selected_teas = []
 
     teas.each do |tea|
-      if tea["type"] == type and tea["stock_quantity"].to_i > 0
+      if types_list.include?(tea["type"])
         selected_teas.push(tea)
       end
     end
 
     selected_teas
+  end
+
+  def use_external_api
+    begin
+      teas = fetch_teas(ENV['TEAS_API_URL'], ENV['AUTHORIZATION_TOKEN'])
+    rescue StandardError
+      error = "Internal Recommendation API Error"
+      selected_teas = []
+    else
+      error = "-"
+      selected_teas = teas_in_stock(teas)
+    end
+
+    return json_response(selected_teas, error)
   end
 
   def json_response(teas_list, error)
